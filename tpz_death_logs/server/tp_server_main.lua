0feeacfeddb8ceef3579a096b233c44aedbb60ef
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-12-05 19:52:34 +0200

    Update tp_server_main.lua

--- tpz_death_logs/server/tp_server_main.lua
+++ tpz_death_logs/server/tp_server_main.lua
@@ -6,8 +6,13 @@ local TPZ = exports.tpz_core:getCoreAPI()
 
 RegisterServerEvent("tpz_death_logs:server:trigger")
 AddEventHandler("tpz_death_logs:server:trigger", function(deathReason, deathCauseHash, killedByPlayer, killedByWeapon)
-  local _source        = source
-  local xPlayer        = TPZ.GetPlayer(_source)
+  local _source = source
+  local xPlayer = TPZ.GetPlayer(_source)
+
+  if not xPlayer.loaded() then 
+    return
+  end
+
   local identifier     = xPlayer.getIdentifier()
   local charIdentifier = xPlayer.getCharacterIdentifier()
   local fullname       = xPlayer.getFirstName() .. ' ' .. xPlayer.getLastName()
@@ -20,8 +25,7 @@ AddEventHandler("tpz_death_logs:server:trigger", function(deathReason, deathCaus
     killedByPlayer   = tfullname .. '( Steam Name: ' .. GetPlayerName(tonumber(killedByPlayer)) .. ', Online Id: ' .. killedByPlayer .. ' )'
   end
 
-  local webhook        = 'webhookhere'
+  local webhook        = 'webhookhere' -- WEBHOOK URL HERE
   local description    = string.format('Death Reason: %s\nDeath Cause Hash: %s\nKilled By: %s\nKilled By Weapon: %s', deathReason, deathCauseHash, killedByPlayer, killedByWeapon)
   TPZ.SendToDiscordWithPlayerParameters(webhook, "", _source, steamName, fullname, identifier, charIdentifier, description, 10038562)
 end)
-
