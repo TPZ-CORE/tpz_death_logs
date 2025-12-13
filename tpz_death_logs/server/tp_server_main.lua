5fe857d66b6a45fdf1bb7a8e3c4adcb922eee3b1
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-12-13 22:37:10 +0200

    Update tp_server_main.lua

--- tpz_death_logs/server/tp_server_main.lua
+++ tpz_death_logs/server/tp_server_main.lua
@@ -7,9 +7,6 @@ local TPZ = exports.tpz_core:getCoreAPI()
 RegisterServerEvent("tpz_death_logs:server:trigger")
 AddEventHandler("tpz_death_logs:server:trigger", function(deathReason, deathCauseHash, killedByPlayer, killedByWeapon)
   local _source = source
-
-  if _source == nil then return end
-
   local xPlayer = TPZ.GetPlayer(_source)
 
   if not xPlayer.loaded() then 
@@ -21,18 +18,22 @@ AddEventHandler("tpz_death_logs:server:trigger", function(deathReason, deathCaus
   local fullname       = xPlayer.getFirstName() .. ' ' .. xPlayer.getLastName()
   local steamName      = GetPlayerName(_source)
 
-  if killedByPlayer ~= nil and GetPlayerName(tonumber(killedByPlayer)) ~= nil then 
+  if killedByPlayer ~= nil then 
+    
+    if GetPlayerName(tonumber(killedByPlayer)) ~= nil then 
 
-    local tPlayer = TPZ.GetPlayer(tonumber(killedByPlayer))
+      local tPlayer = TPZ.GetPlayer(tonumber(killedByPlayer))
 
-    if tPlayer.loaded() then 
-      local tfullname  = tPlayer.getFirstName() .. ' ' .. tPlayer.getLastName()
-      killedByPlayer   = tfullname .. '( Steam Name: ' .. GetPlayerName(tonumber(killedByPlayer)) .. ', Online Id: ' .. killedByPlayer .. ' )'
+      if tPlayer.loaded() then 
+        local tfullname  = tPlayer.getFirstName() .. ' ' .. tPlayer.getLastName()
+        killedByPlayer   = tfullname .. '( Steam Name: ' .. GetPlayerName(tonumber(killedByPlayer)) .. ', Online Id: ' .. killedByPlayer .. ' )'
+      end
+
     end
 
   end
 
-  local webhook        = 'https://discord.com/api/webhooks/1443341878543122664/TS6rkZzk3lKAB1_ArOXAxqe28LqWgbjPNcdk5wE_-MIrUtR6hMzsjpR5ekatJrRtsAss'
+  local webhook        = 'webhookhere'
   local description    = string.format('Death Reason: %s\nDeath Cause Hash: %s\nKilled By: %s\nKilled By Weapon: %s', deathReason, deathCauseHash, killedByPlayer, killedByWeapon)
   TPZ.SendToDiscordWithPlayerParameters(webhook, "", _source, steamName, fullname, identifier, charIdentifier, description, 10038562)
 end)
